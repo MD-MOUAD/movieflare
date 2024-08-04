eb129a6fa8153afd90fd773334f0de995a9fe71b
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-08-04 01:14:05 +0100

    fix cover height

--- src/components/CoverComponent.jsx
+++ src/components/CoverComponent.jsx
@@ -3,12 +3,7 @@ import { Link } from "react-router-dom";
 const CoverComponent = ({item}) => {
   return (
     <Link to={`/`}>
-      {/* <div 
-        className="w-full h-[45vh] bg-cover bg-center bg-no-repeat -z-10 opacity-5 relative"
-        style={{ backgroundImage: `url("${BaseImgPathOriginal}/${item.backdrop_path})"`}}>
-          <img src={`${baseImgPath}/${item.poster_path})`} className="h-1/2" />
-      </div> */}
-      <div className="relative h-[45vh] flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url("${BaseImgPathOriginal}/${item.backdrop_path})")` }}>
+      <div className="relative flex items-center justify-center bg-cover bg-center" style={{ backgroundImage: `url("${BaseImgPathOriginal}/${item.backdrop_path})")` }}>
       <div className="absolute inset-0 bg-cover-gradient"></div>
       <div className="relative max-w-4xl mx-auto p-6 bg-opacity-75 backdrop-filter backdrop-blur-sm rounded-lg">
         <div className="flex flex-col md:flex-row items-center text-white">
