051f9d864ce3a9858384ae4b44712c878d30d2fe
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-08-04 19:16:26 +0100

    fix some styles

--- src/components/CoverComponent.jsx
+++ src/components/CoverComponent.jsx
@@ -1,37 +1,47 @@
 import { BaseImgPathOriginal, baseImgPath } from "../services/api";
 import { Link } from "react-router-dom";
-const CoverComponent = ({item}) => {
+const CoverComponent = ({ item }) => {
   const releaseDate = item?.release_date || item?.first_air_date;
   const title = item?.name || item?.title;
   return (
-    <Link to={`/`}>
-      <div className="relative bg-cover bg-center" style={{ backgroundImage: `url("${BaseImgPathOriginal}/${item.backdrop_path})")` }}>
+    <div
+      className="relative bg-cover bg-center"
+      style={{
+        backgroundImage: `url("${BaseImgPathOriginal}/${item.backdrop_path})")`,
+      }}
+    >
       <div className="absolute inset-0 bg-cover-gradient"></div>
       <div className="relative p-6 md:p-12 rounded-lg">
         <div className="flex items-center text-white w-fit max-w-7xl bg-opacity-75 sm:backdrop-filter sm:backdrop-blur-sm">
-          <img src={`${BaseImgPathOriginal}/${item.poster_path}`} alt="Movie Poster" className="w-28 lg:w-48 rounded-lg shadow-lg opacity-65 max-sm:hidden" />
+          <img
+            src={`${BaseImgPathOriginal}/${item.poster_path}`}
+            alt="Movie Poster"
+            className="w-28 lg:w-48 rounded-lg shadow-lg opacity-65 max-sm:hidden"
+          />
           <div className="mt-6 md:mt-0 ml-3 md:ml-6 text-left">
-            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-cyan-400">{title}</h1>
+            <h1 className="text-2xl md:text-4xl lg:text-5xl font-bold text-cyan-400">
+              {title}
+            </h1>
             <p className="mt-2 md:mt-4 text-sm lg:text-lg max-sm:hidden">
-              {item?.release_date && "Release" || "First air"} Date: <span className="font-semibold">{releaseDate}</span>
+              {(item?.release_date && "Release") || "First air"} Date:{" "}
+              <span className="font-semibold">{releaseDate}</span>
             </p>
             <p className="md:mt-2 text-sm lg:text-lg max-sm:hidden">
               Genre: <span className="font-semibold">Action, Adventure</span>
             </p>
-            <p className="mt-4 text-base line-clamp-2 max-md:hidden">
+            <p className="mt-4 xl:text-lg line-clamp-2 max-md:hidden">
               {item?.overview}
             </p>
             <div className="mt-6">
               <button className="bg-red-600 hover:bg-red-700 text-sm text-white font-semibold py-2 px-4 rounded-lg shadow-lg">
-                Watch Now
+                <Link to={`/`}>Watch Now</Link>
               </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-    </Link>
-  )
-}
+  );
+};
 
 export default CoverComponent;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,40 +5,39 @@ import { useState, useEffect } from "react";
 import { fetchTrending } from "../services/api";
 import fakeTrendingData from "../data/data";
 
-
 const Home = () => {
   const [loading, setLoading] = useState(true);
   const [data, setData] = useState([]);
   const [timeInterval, setTimeInterval] = useState("day");
   const [mediaType, setMediaType] = useState("all");
 
-  // useEffect(() => {
-  //   setLoading(true);
-  //   fetchTrending(timeInterval, mediaType)
-  //   .then((results) => {
-  //     setData(results);
-  //   })
-  //   .catch((err) => {
-  //     console.log("Error fetching data:", err);
-  //   })
-  //   .finally(() => {
-  //     setLoading(false);
-  //   })
-  // }, [timeInterval, mediaType]);
+  useEffect(() => {
+    setLoading(true);
+    fetchTrending(timeInterval, mediaType)
+      .then((results) => {
+        setData(results);
+      })
+      .catch((err) => {
+        console.log("Error fetching data:", err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, [timeInterval, mediaType]);
 
   // use fake data
 
-  useEffect(() => {
-    setLoading(true);
-    setData(fakeTrendingData(timeInterval));
-    setTimeout(() => {
-      setLoading(false);
-    }, 1000)
-  }, [timeInterval]);
+  // useEffect(() => {
+  //   setLoading(true);
+  //   setData(fakeTrendingData(timeInterval));
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 1000)
+  // }, [timeInterval]);
 
   return (
     <div className="py-1">
-      {data?.length > 0 && (<CoverComponent item={data[0]}/>)}
+      {data?.length > 0 && <CoverComponent item={data[0]} />}
       <div className="flex items-center space-x-3 py-4 px-4">
         <h2 className="uppercase text-xl max-sm:text-lg">Trending</h2>
         <select
@@ -51,15 +50,19 @@ const Home = () => {
           <option value="movie">Movies</option>
           <option value="tv">TV Shows</option>
         </select>
-        <div className="flex border-4 border-zinc-200 dark:border-white/5 rounded-full">
-          <button 
-            className={`px-5 max-sm:px-2 rounded-full ${timeInterval == "day" ?"bg-cyan-600 text-slate-100" : ""} transition-all duration-300 shrink-0`}
+        <div className="flex border border-black dark:border-white rounded-full shadow-md">
+          <button
+            className={`px-5 max-sm:px-2 rounded-full ${
+              timeInterval == "day" ? "bg-cyan-600 text-slate-100" : ""
+            } transition-all duration-300 shrink-0`}
             onClick={() => setTimeInterval("day")}
           >
             Today
           </button>
-          <button 
-            className={`px-5 max-sm:px-2 py-1 rounded-full ${timeInterval == "week" ?"bg-cyan-600 text-slate-100" : ""} transition-all duration-300 shrink-0`}
+          <button
+            className={`px-5 max-sm:px-2 py-1 rounded-full ${
+              timeInterval == "week" ? "bg-cyan-600 text-slate-100" : ""
+            } transition-all duration-300 shrink-0`}
             onClick={() => setTimeInterval("week")}
           >
             This Week
@@ -67,13 +70,13 @@ const Home = () => {
         </div>
       </div>
       <div className="flex overflow-x-auto gap-5 py-3 px-1 mx-4 max-sm:py-1 max-sm:mx-0 max-sm:scrollbar-none">
-        {
-          loading
-          ? [...Array(19)].map((_, i) => <Skeleton key={i} />) 
-          : data?.map((item, i) => i > 0 && <CardComponent key={item.id} item={item} />)
-        }
+        {loading
+          ? [...Array(19)].map((_, i) => <Skeleton key={i} />)
+          : data?.map(
+              (item, i) => i > 0 && <CardComponent key={item.id} item={item} />
+            )}
       </div>
-    </div> 
+    </div>
   );
 };
 
