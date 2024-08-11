e264486f80a373b8961b210cc426bf296b8683e1
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-08-12 00:45:48 +0100

    update logo color

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ import { BiCameraMovie } from "../utils/icons";
 
 const Logo = () => {
   return (
-    <div className="flex items-center text-red-700">
+    <div className="flex items-center text-red-500 dark:text-red-400">
       <BiCameraMovie className="text-4xl max-sm:text-3xl" />
       <h1 className="text-2xl max-sm:text-xl font-bold font-raleway bg-gradient-to-r from-red-400 via-red-900 to-red-600 bg-clip-text text-transparent tracking-wide">
         <span className="text-3xl max-sm:text-2xl">M</span>ovie
