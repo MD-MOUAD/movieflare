3bb17c9bad3780ab4c5204c7e3fce51a85519fce
MD-MOUAD <mouadmouadkhanouch@gmail.com>
2024-08-31 22:30:44 +0100

    handle mobile search

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -12,6 +12,13 @@ const SearchBar = () => {
     setDebouncedQuery("");
     setQuery("");
     setSearchResults([]);
+    document.querySelector('#search-bar').classList.add("max-sm:hidden");
+    document.querySelector('#mobile-search-bar-arrow').classList.add("max-sm:hidden");
+  };
+
+  const handleMobileSearch = () => {
+    document.querySelector('#search-bar').classList.remove("max-sm:hidden");
+    document.querySelector('#mobile-search-bar-arrow').classList.remove("max-sm:hidden");
   };
 
   useEffect(() => {
@@ -39,43 +46,59 @@ const SearchBar = () => {
   }, [debouncedQuery]);
 
   return (
-    <div className="mx-5 px-5 h-10 rounded-lg max-w-96  flex justify-center items-center gap-2 bg-slate-300 dark:bg-gray-50 focus-within:ring-2 ring-black dark:ring-red-600 shadow-lg text-black dark:text-gray-900 relative z-10 group max-sm:hidden">
-      <input
-        id="searchBar"
-        type="text"
-        placeholder="Search on website"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        className="px-5 h-full w-4/6 bg-inherit outline-none flex-1 placeholder:text-gray-600 dark:placeholder:text-inputColor"
-      />
-      <button
-        className={`invisible ${debouncedQuery !== "" &&"group-focus-within:visible"} cursor-pointer opacity-35 hover:scale-105 hover:opacity-100`}
-        onClick={handleClear}
-      >
-        <FaTimes />
-      </button>
-      <FaSearch
-        className="text-red-500"
-        size={20}
-      />
-      <ul className="hidden absolute top-full left-0  w-[140%] mt-2 rounded-lg bg-slate-200 dark:bg-neutral-900  max-h-[50vh] z-10 group-focus-within:block overflow-auto">
-        {searchResults?.map((item, i) => {
-          if (!["movie", "tv"].includes(item.media_type)) {
-            return;
-          }
-          const title = item.name || item.title;
-          const posterPath = baseImgPath + item.poster_path;
-          return (
-            <Link to={`${item.media_type}/${item.id}`} key={i}>
-              <li className="flex gap-2 p-2 hover:bg-zinc-300 dark:hover:bg-zinc-400 text-gray-400 hover:text-black">
-                <img src={posterPath} alt={title} className="w-12" />
-                <div className="line-clamp-1 font-bold">{title}</div>
-              </li>
-            </Link>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div id="search-bar" className="px-5 h-10 rounded-lg sm:max-w-96 flex justify-center items-center gap-2 bg-slate-300 dark:bg-gray-50 sm:focus-within:ring-2 ring-black dark:ring-red-600 shadow-lg text-black dark:text-gray-900 relative z-10 max-sm:absolute max-sm:top-full max-sm:left-0 max-sm:w-screen max-sm:h-12 max-sm:hidden group">
+        <input
+          id="searchBar"
+          type="text"
+          placeholder="Search on website"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          className="px-5 h-full w-4/6 bg-inherit outline-none flex-1 placeholder:text-gray-600 dark:placeholder:text-inputColor max-sm:rounded-lg"
+        />
+        <button
+          className={`sm:invisible ${
+            debouncedQuery !== "" && "group-focus-within:visible"
+          } cursor-pointer sm:opacity-35 hover:scale-105 hover:opacity-100`}
+          onClick={handleClear}
+        >
+          <FaTimes />
+        </button>
+        <FaSearch className="text-red-500 max-sm:hidden" size={20} />
+        <ul className="hidden absolute top-full left-0  w-[140%] max-sm:w-full sm:mt-2 rounded-lg bg-slate-200 dark:bg-neutral-900  max-h-[50vh] z-10 group-focus-within:block overflow-auto">
+          {searchResults?.map((item, i) => {
+            if (!["movie", "tv"].includes(item.media_type)) {
+              return;
+            }
+            const title = item.name || item.title;
+            const posterPath = baseImgPath + item.poster_path;
+            return (
+              <Link to={`${item.media_type}/${item.id}`} key={i}>
+                <li
+                  className="flex gap-2 p-2 hover:bg-zinc-300 dark:hover:bg-zinc-400 text-gray-400 hover:text-black"
+                  onClick={handleClear}
+                >
+                  <img
+                    src={posterPath}
+                    alt={"Poster"}
+                    className="w-12 aspect-card"
+                    onError={(e) =>
+                      (e.currentTarget.src = "https://via.placeholder.com/400")
+                    }
+                  />
+                  <div className="line-clamp-2 font-bold">{title}</div>
+                </li>
+              </Link>
+            );
+          })}
+        </ul>
+      </div>
+      {/* for mobile */}
+      <div className="relative text-red-500 sm:hidden cursor-pointer">
+        <FaSearch size={24} onClick={handleMobileSearch} />
+        <div id="mobile-search-bar-arrow" className="size-6 absolute top-9 left-1/2 -translate-x-1/2 rotate-45 bg-slate-300 dark:bg-gray-50  max-sm:hidden"></div>
+      </div>
+    </>
   );
 };
 
